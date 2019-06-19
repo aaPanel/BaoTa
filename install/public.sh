192c88a00b94e5d8378c019802b92deb3cd4f4c1
bt.cn <287962566@qq.com>
2019-06-19 17:40:50 +0800

    6.9.23

--- install/public.sh
+++ install/public.sh
@@ -5,7 +5,7 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US:en
 
 get_node_url(){
-	nodes=(http://183.235.223.101:3389 http://125.88.182.172:5880 http://128.1.164.196 http://103.224.251.67 http://download.bt.cn);
+	nodes=(http://183.235.223.101:3389 http://119.188.210.21:5880 http://125.88.182.172:5880 http://103.224.251.67 http://download.bt.cn http://45.32.116.160 http://128.1.164.196);
 	i=1;
 	for node in ${nodes[@]};
 	do
