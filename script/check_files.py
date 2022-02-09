6ae3d1db05dd358eb95f74816b09e9ae4965b666
宝塔Linux开发 - 阿良 <287962566@qq.com>
2022-02-09 17:11:10 +0800

    修改文件完整性检测域名

--- script/check_files.py
+++ script/check_files.py
@@ -6,7 +6,7 @@ import public
 import http_requests
 http_requests.DEFAULT_TYPE = 'src'
 
-url1 = 'https://www.bt.cn/api/panel/check_files'
+url1 = 'https://check.bt.cn/api/panel/check_files'
 pdata = {'panel_version': public.version(), 'address': public.get_ipaddress()}
 result = http_requests.post(url1, pdata).text
 print(result)
